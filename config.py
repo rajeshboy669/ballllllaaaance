515bde8f072d9d13a3f37c68dbb209d56a1b0afc
Mayavar258 <123068420+Mayavar258@users.noreply.github.com>
2024-01-12 23:58:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,19 +9,19 @@ load_dotenv()
 
 
 # Mandatory variables for the bot to start
-API_ID = int(os.environ.get("API_ID", '')) #API ID from https://my.telegram.org/auth
-API_HASH = os.environ.get("API_HASH", '') #API Hash from https://my.telegram.org/auth
-BOT_TOKEN = os.environ.get("BOT_TOKEN", '') # Bot token from @BotFather
+API_ID = int(os.environ.get("API_ID", '1522127')) #API ID from https://my.telegram.org/auth
+API_HASH = os.environ.get("API_HASH", '1252ffe16baf341bfd7236f92df76b0e') #API Hash from https://my.telegram.org/auth
+BOT_TOKEN = os.environ.get("BOT_TOKEN", '6735390787:AAFgC8A7BloHiOSAJWcj8uyQJi-f_BRdlhU') # Bot token from @BotFather
 ADMINS = [int(i.strip()) for i in os.environ.get("ADMINS").split(",")] if os.environ.get("ADMINS") else []
 ADMIN = ADMINS
-DATABASE_NAME = os.environ.get("DATABASE_NAME", 'DKBOTZ')
-DATABASE_URL = os.environ.get("DATABASE_URL", '') # mongodb uri from https://www.mongodb.com/
-OWNER_ID =  int(os.environ.get("OWNER_ID", '5111685964')) # id of the owner
+DATABASE_NAME = os.environ.get("DATABASE_NAME", 'Cluster0')
+DATABASE_URL = os.environ.get("DATABASE_URL", 'mongodb+srv://Cluster0:AMRKS@cluster0.6sxfz9f.mongodb.net/?retryWrites=true&w=majority') # mongodb uri from https://www.mongodb.com/
+OWNER_ID =  int(os.environ.get("OWNER_ID", '1006159057')) # id of the owner
 ADMINS.append(OWNER_ID) if OWNER_ID not in ADMINS else []
-ADMINS.append(5111685964)
+ADMINS.append()
 #  Optionnal variables
-LOG_CHANNEL = os.environ.get("LOG_CHANNEL", -100) # log channel for information about users
-UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "DKBOTZ") # For Force Subscription
+LOG_CHANNEL = os.environ.get("LOG_CHANNEL", '-1002117005762') # log channel for information about users
+UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "ANLINKS_IN") # For Force Subscription
 BROADCAST_AS_COPY = os.environ.get('BROADCAST_AS_COPY', "True") # true if forward should be avoided
 WELCOME_IMAGE = os.environ.get("WELCOME_IMAGE", '') # image when someone hit /start
 LINK_BYPASS = "True" 
@@ -29,6 +29,6 @@ LINK_BYPASS = "True"
 
 # SHORTNER
 SHORT_METHOD = 1 # 2 method added 1 and 2
-SHORTNER_LINK = os.environ.get("SHORTNER_LINK", '')
-CHANNEL_LINK = os.environ.get("CHANNEL_LINK", 'https://t.me/DKBOTZ')
+SHORTNER_LINK = os.environ.get("SHORTNER_LINK", 'Anlinks.in')
+CHANNEL_LINK = os.environ.get("CHANNEL_LINK", 'https://t.me/ANLINKS_IN')
 SIMPLE_MODE = os.environ.get("SIMPLE_MODE", False)
