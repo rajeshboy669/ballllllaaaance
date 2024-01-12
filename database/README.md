f85e55caa629be92e6b22269474928a8462b38af
Mayavar258 <123068420+Mayavar258@users.noreply.github.com>
2024-01-12 06:05:24 +0000

    Add files via upload

---
+++ database/README.md
@@ -0,0 +1,2 @@
+# DKBOTZ
+Codes By [DKBOTZ](https://t.me/DKBOTZ)
---
+++ database/__init__.py
@@ -0,0 +1,6 @@
+# ©️ DKBOTZ or https://t.me/DKBOTZ
+# Coded By https://t.me/DKBOTZHELP 
+# Don't Edit
+
+from .database import db
+from .users import get_all_users, delete_user, get_user, update_user_info, total_users_count
