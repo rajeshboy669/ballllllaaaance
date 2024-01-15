834ad7f22e70b2f55e3c932a2487688ecc89e4ad
༒ᶜʳᵃᶻʸᴮᴼˢˢ卂乃卄丨丂卄乇Ҝ༒ <93928612+Abhishek-Issac@users.noreply.github.com>
2024-01-15 20:28:03 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -143,12 +143,20 @@ async def on_callback_query(bot: Client, query: CallbackQuery):
         await query.message.edit(ABOUT_TEXT.format(bot.mention(style='md')), reply_markup=ABOUT_REPLY_MARKUP, disable_web_page_preview=True)
 
     elif query.data == 'start_dkbotz':
+        R_REPLY_MARKUP = [
+                [
+                    InlineKeyboardButton("Update Channel", url="https://t.me/good_nation"),
+                    InlineKeyboardButton("Support 🤝", callback_data="help_dkbotz"),
+                ],[
+                    InlineKeyboardButton("Connect To Anlinks🛠️", callback_data="about_dkbotz")
+                ]
+        ]
         new_user = await get_user(query.from_user.id)
         tit = START_MESSAGE.format(user=query.from_user.mention, method=new_user["method"], site=new_user["base_site"])
         if SIMPLE_MODE:
-            await m.reply_text(tit, reply_markup=SIMPLE_START_MESSAGE_REPLY_MARKUP, disable_web_page_preview=True)
+            await m.reply_text(tit, reply_markup=R_REPLY_MARKUP, disable_web_page_preview=True)
         else:
-            await query.message.edit(tit, reply_markup=START_MESSAGE_REPLY_MARKUP, disable_web_page_preview=True)
+            await query.message.edit(tit, reply_markup=R_REPLY_MARKUP, disable_web_page_preview=True)
     elif query.data == 'new_btn_dkbotz':
         new_user = await get_user(query.from_user.id)
         tit = START_MESSAGE.format(user=query.from_user.mention, method=new_user["method"], site=new_user["base_site"])
