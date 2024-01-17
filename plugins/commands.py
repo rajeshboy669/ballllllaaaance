8e024ee24b60ce6eaf414355039956781bc12452
༒ᶜʳᵃᶻʸᴮᴼˢˢ卂乃卄丨丂卄乇Ҝ༒ <93928612+Abhishek-Issac@users.noreply.github.com>
2024-01-17 06:02:23 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -24,30 +24,21 @@ logger = logging.getLogger(__name__)
 
 @Client.on_message(filters.command('start'))
 async def startcmdssss_by_dkbotz(c, m:Message):
-
-    SER_REPLY_MARKUP = [
-                [
-                    InlineKeyboardButton("Update Channel", url="https://t.me/good_nation"),
-                    InlineKeyboardButton("Support 🤝", callback_data="help_dkbotz"),
-                ],[
-                    InlineKeyboardButton("Connect To Anlinks🛠️", callback_data="about_dkbotz")
-                ]
-            ]
     is_user = await is_user_exist(m.from_user.id)
 
-    reply_markup = InlineKeyboardMarkup(SER_REPLY_MARKUP)
+    reply_markup = R_REPLY_MARKUP
 
     if not is_user and LOG_CHANNEL: await c.send_message(LOG_CHANNEL, f"#NewUser\n\nUser ID: `{m.from_user.id}`\nName: {m.from_user.mention}", reply_markup=reply_markup)
     new_user = await get_user(m.from_user.id)  
 
     t = START_MESSAGE.format(user=m.from_user.mention, method=new_user["method"], site=new_user["base_site"])
     if WELCOME_IMAGE:
-        return await m.reply_photo(photo=WELCOME_IMAGE, caption=t, reply_markup=SER_REPLY_MARKUP)
+        return await m.reply_photo(photo=WELCOME_IMAGE, caption=t, reply_markup=R_REPLY_MARKUP)
 
     if SIMPLE_MODE:
-        await m.reply_text(t, reply_markup=SER_REPLY_MARKUP, disable_web_page_preview=True)
+        await m.reply_text(t, reply_markup=R_REPLY_MARKUP, disable_web_page_preview=True)
     else:
-        await m.reply_text(t, reply_markup=SER_REPLY_MARKUP, disable_web_page_preview=True)
+        await m.reply_text(t, reply_markup=R_REPLY_MARKUP, disable_web_page_preview=True)
 
 @Client.on_message(filters.command('help') & filters.private)
 async def help_command_by_dkbotz(c, m: Message):
@@ -91,7 +82,7 @@ async def balance_command_by_dkbotz(c, m: Message):
 
 @Client.on_message(filters.command('about'))
 async def about_command(c, m: Message):
-    reply_markup=ABOUT_REPLY_MARKUP
+    reply_markup=BACK_REPLY_MARKUP
 
     bot = await c.get_me()
     if WELCOME_IMAGE:
