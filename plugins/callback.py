0e44a5dee52ff74a2d65096a9a20a8299cb91ebe
༒ᶜʳᵃᶻʸᴮᴼˢˢ卂乃卄丨丂卄乇Ҝ༒ <93928612+Abhishek-Issac@users.noreply.github.com>
2024-01-17 20:28:50 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -24,10 +24,10 @@ logger = logging.getLogger(__name__)
 keyboard = [
     [
         InlineKeyboardButton("Update Channel", url="https://t.me/good_nation"),
-        InlineKeyboardButton("Support 🤝", callback_data="about_dkbotz"),
+        InlineKeyboardButton("Support 🤝", callback_data="help_dkbotz"),
     ],
     [
-        InlineKeyboardButton("Connect To Anlinks🛠️", callback_data="help_dkbotz")
+        InlineKeyboardButton("Connect To Anlinks🛠️", callback_data="about_dkbotz")
     ]
 ]
 
