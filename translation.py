dc91d5aa982803efe9bcc80228450ddd4ae0edc9
Mayavar258 <123068420+Mayavar258@users.noreply.github.com>
2024-01-21 07:55:39 +0530

    Add files via upload

--- translation.py
+++ translation.py
@@ -5,7 +5,7 @@ from config import *
 
 SIMPLE_START_MESSAGE_REPLY_MARKUP = InlineKeyboardMarkup([
     [
-        InlineKeyboardButton('📡 Connect To Anlinks ', url=f"https://{SHORTNER_LINK}/member/tools/api?bot=true")
+        InlineKeyboardButton('📡 Connect To Bot', url=f"https://{SHORTNER_LINK}/member/tools/api?bot=true")
     ]
 ])
 
@@ -44,15 +44,62 @@ I Am {site} , Bulk Link Converter. I Can Convert Links Directly From Your {site}
 
 3. Than Type /set_api than give a single space and than paste your API Key (see example to understand more...)
 
+/set_api(space)API Key 
 (See Example.👇)
 Example:** `/set_api cbd63775f798fe0e58c67a56e6ce8b70c495cda4 `
 
-⭐️ If you need any help or Support Contact Us at @AnLinks_in_support 
+**💁‍♀️ Hit 👉 /help To Get Help.
 
+➕ Hit 👉 /footer To Get Help About Adding your Custom Footer to bot.
+
+➕ Hit 👉 /header To Get Help About Adding your Custom Footer to bot.**
 '''
 
+HELP_MESSAGE = '''
+**Hey! My name is {firstname}. I am a {username}.**
+
+Features 
+
+- [Hyperlink](https://t.me/{username})
+- Buttons convert support
+- Header and Footer Text support
+- Replace Username
+- Banner Image
+
+Helpful commands:
+
+- /start: Starts me! You've probably already used this.
+- /help: Sends this message; I'll tell you more about myself!
+If You Have Any Problem Then Contact - @DKBOTZHELP_2
 
 Available commands:
+
+- /set_api - Use This Cmd To Add API
+- /header - Set Header Text
+- /footer - Set Footer Text
+- /username - Set Username
+- /banner_image - Set A Banner For Post
+- /me - Your Settings Details
+- /balance - Get Your Balance Info
+- /features - Get Features Info
+
+Use the commands to know more about the same
+Below are some features I provide'''
+
+
+FEATURES_MESSAGE = '''
+**Hey! My name is {firstname}. I am a {username}.
+
+Available commands:
+
+- /set_api - Use This Cmd To Add API
+- /header - Set Header Text
+- /footer - Set Footer Text
+- /username - Set Username
+- /banner_image - Set A Banner For Post
+- /me - Your Settings Details
+- /balance - Get Your Balance Info
+- /features - Get Features Info**'''
 
 ABOUT_TEXT = """
 **My Details:**
@@ -62,11 +109,11 @@ ABOUT_TEXT = """
 
 `🧰 Framework:` [Pyrogram](https://github.com/pyrogram/pyrogram)
 
-`👨‍💻 Developer:` [Anonymous](https://t.me/Anlinks_in_support)
+`👨‍💻 Developer:` [Anonymous](t.me/DKBOTZHELP_2)
 
-`📢 Support:` [Anonymous](https://t.me/ANLINKS_IN)
+`📢 Support:` [Anonymous](https://t.me/DKBOTZ)
 
-`🌐 Source Code:` **[Click Here](https://t.me/Anlinks_in_support)**
+`🌐 Source Code:` **[Click Here](https://t.me/DKBOTZHELP_2)**
 """
 
 
@@ -190,4 +237,5 @@ Usage: `/unban [User ID]`
 
 List of users that are banned:
 
-{users}`"""
+{users}
+"""
